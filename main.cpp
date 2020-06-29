d7bb79ab97ab65b1f8b0f7e30ada6021170b9769
AmanoKokoro <55781271+AmanoKokoro@users.noreply.github.com>
2020-06-29 16:46:08 +0900

    first comit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include "main.hpp"
+
+
+//Name space
+using namespace cv;
+using namespace std;
+
+// global
+string win_src = "src";
+string win_dst = "dst";
+
+int main(void) {
+	string file_src = "img/hoge.jpg";	//input image path
+	string file_dst = "img/foobar.jpg";	//output_image path
+
+	Mat img_src = imread(file_src, 1);		//カラー画像として入力
+	//Mat img_src = imread(file_src, 0);	//0でグレースケール
+
+	Mat img_dst;	//画像処理オブジェクト
+	
+	/*ファイルオープンチェック*/
+	if (!img_src.data) {
+		cout << "Input error!!" << endl;
+		return -1;
+	}
+
+	namedWindow(win_src, WINDOW_AUTOSIZE);	//input image window
+	namedWindow(win_dst, WINDOW_AUTOSIZE);	//output image window
+
+	/*Operation*/
+	Mat op = Mat::ones(3, 3, CV_32F) / 9.0;	//全要素が1の3*3フィルタを生成 9で割る (全要素が1/9の3*3行列)
+	filter2D(img_src, img_dst, -1, op);
+	convertScaleAbs(img_dst, img_dst, 1, 0);
+
+	return 0;
+}
---
+++ main.hpp
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/*Include Library*/
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <opencv2/opencv.hpp>
+//#include <opencv2/highgui/highgui_c.h>
+
+#endif // !_MAIN_H
